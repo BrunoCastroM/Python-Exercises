edb1a2dd6ab498df7118128e98313f39f33d4149
Bruno Castro <brunocastro8@hotmail.com>
2022-06-23 10:14:47 -0300

    I changed the file name

--- Teste da aula 10.py
+++
@@ -1,8 +0,0 @@
-n1=float(input('Qual foi a sua primeira nota: '))
-n2=float(input('Qual foi sua segunda nota: '))
-m=(n1+n2)/2
-print('Sua média é: {:.2f}'.format(m))
-if m>=6:
-    print('Você está bem!')
-else:
-    print('Você está passando mal!')
